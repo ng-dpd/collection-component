3517610031ea82b7785f52ba96904331e9cb1042
Jeff Cross <middlefloor@gmail.com>
2014-01-12 22:32:12 -0800

    feat(query): add auto query method to component and controller

--- src/collection-component.js
+++ src/collection-component.js
@@ -1,13 +1,31 @@
 angular.module('dpdCollection', []).
-  controller('CollectionComponentCtrl', ['$scope', function ($scope) {
+  controller('CollectionComponentCtrl',
+      ['$scope', '$http', function ($scope, $http) {
+        this.onGetCollection = function (coll) {
+        };
+
+        this.onGetCollectionError = function (coll, err) {
+        };
+
+        this.query = function () {
+          if ($scope.collectionPath) {
+            $http.get($scope.collectionPath).
+            success(this.onGetCollection).
+            error(this.onGetCollectionError);
+          }
+        };
   }]).
   directive('dpdCollection', ['$parse', function ($parse) {
-
     return {
       restrict: 'E',
       templateUrl: 'collection-component.html',
-      link: function (scope, element, attrs) {
+      controller: 'CollectionComponentCtrl',
+      controllerAs: 'collectionCtrl',
+      link: function (scope, element, attrs, ctrl) {
         scope.properties = $parse(attrs.collectionProperties)(scope);
+        scope.collectionPath = $parse(attrs.collectionPath)(scope);
+
+        ctrl.query();
       }
-    }
+    };
   }]);
--- test/collection-component.spec.js
+++ test/collection-component.spec.js
@@ -1,17 +1,41 @@
 describe('dpdCollection', function () {
-  var $compile, $rootScope;
+  var $compile, $rootScope, $controller, $httpBackend;
 
   beforeEach(module('dpdCollection', 'collection-component.html'));
 
-  beforeEach(inject(function (_$compile_, _$rootScope_) {
+  beforeEach(inject(function (_$compile_, _$rootScope_, _$controller_, _$httpBackend_) {
     $compile = _$compile_;
     $rootScope = _$rootScope_;
+    $controller = _$controller_;
+    $httpBackend = _$httpBackend_;
   }));
+
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
 
   describe('collectionComponent', function () {
     it('should show loading row before data is fetched', function () {
 
+    });
+
+
+    it('should set properties on the scope from a passed-in array literal', function () {
+      var originalProp = [{key:'foo', readable: 'foo readable'}];
+      var collectionProp = JSON.stringify(originalProp).replace(/\"/g, '\'');
+      var compiled = $compile(
+          '<dpd-collection ' +
+          'collection-properties="'+
+          collectionProp +
+          '"></dpd-collection>'
+          )($rootScope);
+      $rootScope.$digest();
+      delete $rootScope.properties[0].$$hashKey;
+
+      expect($rootScope.properties).toEqual(originalProp);
     });
 
 
@@ -28,11 +52,36 @@ describe('dpdCollection', function () {
     });
 
 
-    it('should enforce property order')
+    it('should set collectionPath on scope when passed an array literal', function () {
+      $httpBackend.expectGET('/myCollection').respond(200);
+      var compiled = $compile(
+          '<dpd-collection ' +
+          'collection-path="'+
+          '\'/myCollection\''+
+          '"></dpd-collection>'
+          )($rootScope);
+      $rootScope.$digest();
+
+      expect($rootScope.collectionPath).toBe('/myCollection');
+      $httpBackend.flush();
+    });
   });
 
 
   describe('CollectionComponentCtrl', function () {
+    describe('.query()', function () {
+      it('should request the collection once `collectionPath` is set', function () {
+        var scope = $rootScope.$new();
+        var controller = $controller('CollectionComponentCtrl', {$scope: scope});
+        $httpBackend.expectGET('/myCollection').respond(200);
+        scope.collectionPath = '/myCollection';
+        controller.query();
+        $httpBackend.flush();
+      });
+    });
 
   });
+
+
+  it('should allow auto updating of rows');
 });
